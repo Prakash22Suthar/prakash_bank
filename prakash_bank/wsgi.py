979c3cd09888e0c8920037f79c290d274bf4221a
prakash22suthar <prakash22suthar@gmail.com>
2024-08-08 23:25:15 +0530

    important changes

--- prakash_bank/wsgi.py
+++ prakash_bank/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for r_bank project.
+WSGI config for prakash_bank project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'r_bank.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'prakash_bank.settings')
 
 application = get_wsgi_application()
